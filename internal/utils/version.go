8317e68a88c815e340921fc885978a66ea337384
gnomegl <165720628+gnomegl@users.noreply.github.com>
2024-12-28 11:18:55 -0500

    fix: make version variable public for ldflags

--- internal/utils/version.go
+++ internal/utils/version.go
@@ -5,15 +5,15 @@ import (
 	"strings"
 )
 
-// version will be set by GoReleaser during builds
-var version string
+// Version will be set by GoReleaser during builds
+var Version string
 
 // GetVersion returns the current version of the application.
 // If version is not set via ldflags, it will try to get it from Go's build info.
 // If that fails, it returns "unknown".
 // The version string will have any leading "v" prefix removed.
 func GetVersion() string {
-	v := version
+	v := Version
 	if v == "" {
 		if info, ok := debug.ReadBuildInfo(); ok {
 			v = info.Main.Version
