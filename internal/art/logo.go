1bba8c1fadec475fa4a4a964ce4fba8cc713fd1a
gnomegl <gnomegl@users.noreply.github.com>
2024-12-28 00:44:46 -0500

    Better version handling in Makefile and update logo display in art package

--- internal/art/logo.go
+++ internal/art/logo.go
@@ -1,17 +1,26 @@
 package art
 
-import "github.com/fatih/color"
+import (
+	"fmt"
 
-const Logo = `   _____   _   _____  _                    
+	"github.com/fatih/color"
+)
+
+// based: injected at build time
+var version = "dev"
+
+const LogoMain = `   _____   _   _____  _                    
   / ____(_) | /  ___)| |                   
  | |  __| | |_| (___ | |_   _ _ __ _ __  
  | | |_ | | __|\___ \| | | | | '__| '_ \ 
  | |__| | | |_ ____) | | |_| | |  | |_) |
   \_____|_|\__|_____/|_|\__,_|_|  | .__/ 
                                   | |    
-                                  |_|    `
+                                  |_|    %s`
+
+const LogoText = "v%s by gnomegl"
 
 // PrintLogo prints the gitslurp logo in color
 func PrintLogo() {
-	color.Cyan(Logo)
+	color.Cyan(fmt.Sprintf(LogoMain, color.HiYellowString(fmt.Sprintf(LogoText, version))))
 }
