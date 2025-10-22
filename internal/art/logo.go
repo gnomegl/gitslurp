872489bc69ad372d385a27cd47b34c6e7fa0a2ce
gnomegl <165720628+gnomegl@users.noreply.github.com>
2025-10-21 22:29:22 -0400

    feat: enhance logo with go-figure ASCII art generator

--- internal/art/logo.go
+++ internal/art/logo.go
@@ -4,21 +4,12 @@ import (
 	"fmt"
 	"os"
 
-	"git.sr.ht/~gnome/gitslurp/internal/utils"
+	"github.com/common-nighthawk/go-figure"
+	"github.com/gnomegl/gitslurp/internal/utils"
 )
 
-const LogoMain = `   _____   _   _____  _                    
-  / ____(_) | /  ___)| |                   
- | |  __| | |_| (___ | |_   _ _ __ _ __  
- | | |_ | | __|\___ \| | | | | '__| '_ \ 
- | |__| | | |_ ____) | | |_| | |  | |_) |
-  \_____|_|\__|_____/|_|\__,_|_|  | .__/ 
-                                  | |    
-                                  |_|  %s`
-
-const LogoText = "v%s by @0xGnomeGL"
-
 func PrintLogo() {
-	fmt.Fprintf(os.Stderr, "\033[36m%s\033[0m\n", fmt.Sprintf(LogoMain, fmt.Sprintf("\033[91m"+LogoText+"\033[0m", utils.GetVersion())))
-	fmt.Fprintln(os.Stderr)
+	myFigure := figure.NewFigure("gitslurp", "chunky", false)
+	fmt.Fprintf(os.Stderr, "\033[36m%s\033[0m", myFigure.String())
+	fmt.Fprintf(os.Stderr, "              \033[91mv%s by gnomegl\033[0m\n\n", utils.GetVersion())
 }
